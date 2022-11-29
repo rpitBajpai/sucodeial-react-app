815beb026b801dfc9922365a6d14beb526a1ad9f
Arpit Bajpai <arpitbajpai4321@gmail.com>
2022-11-29 15:29:38 +0530

    Custom Fetch

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,16 @@
-
+import { useEffect } from "react";
+import { getPosts } from '../api';
 
 
 function App() {
+  useEffect(() => {
+    const fetchPosts = async () => {
+      const response = await getPosts();
+      console.log('response', response);
+    };
+    fetchPosts();
+  }, []);
+  
   return (
     <div className="App">
       <h1>Codeial</h1>
