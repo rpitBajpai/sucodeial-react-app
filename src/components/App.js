361c55cc30a9b8a5787a9e5e9ea606af6a950f4a
Arpit Bajpai <arpitbajpai4321@gmail.com>
2022-11-30 20:12:01 +0530

    useAuth() in Navbar

--- src/components/App.js
+++ src/components/App.js
@@ -1,18 +1,8 @@
-import { useEffect, useState } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
-import { getPosts } from '../api';
 import { useAuth } from '../hooks';
 import { Home, Login } from '../pages';
 import { Loader, Navbar } from './';
-
-const About = () => {
-  return <h1>About</h1>;
-};
-
-const UserInfo = () => {
-  return <h1>User</h1>;
-};
 
 const Page404 = () => {
   return <h1>404</h1>;
@@ -20,20 +10,6 @@ const Page404 = () => {
 
 function App() {
   const auth = useAuth();
-
-  // useEffect(() => {
-  //   const fetchPosts = async () => {
-  //     const response = await getPosts();
-
-  //     if (response.success) {
-  //       setPosts(response.data.posts);
-  //     }
-
-  //     setLoading(false);
-  //   };
-
-  //   fetchPosts();
-  // }, []);
 
   if (auth.loading) {
     return <Loader />;
@@ -45,19 +21,11 @@ function App() {
         <Navbar />
         <Switch>
           <Route exact path="/">
-            <Home posts={[]} />
+            <Home />
           </Route>
 
           <Route exact path="/login">
             <Login />
-          </Route>
-
-          <Route exact path="/about">
-            <About />
-          </Route>
-
-          <Route exact path="/user/asdasd">
-            <UserInfo />
           </Route>
 
           <Route>
