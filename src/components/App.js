29163e8a48b66ab6ad0b21293f03ced4c88e3922
Arpit Bajpai <arpitbajpai4321@gmail.com>
2022-11-30 14:18:33 +0530

    Adding Routes

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,21 @@
 import { useEffect, useState } from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 import { getPosts } from '../api';
-import { Home } from '../pages';
+import { Home, Login } from '../pages';
 import { Loader, Navbar } from './';
+
+const About = () => {
+  return <h1>About</h1>;
+};
+
+const UserInfo = () => {
+  return <h1>User</h1>;
+};
+
+const Page404 = () => {
+  return <h1>404</h1>;
+};
 
 function App() {
   const [posts, setPosts] = useState([]);
@@ -29,7 +42,27 @@ function App() {
   return (
     <div className="App">
       <Navbar />
-      <Home posts={posts} />
+      <Router>
+        <Route exact path="/">
+          <Home posts={posts} />
+        </Route>
+
+        <Route exact path="/login">
+          <Login />
+        </Route>
+
+        <Route exact path="/about">
+          <About />
+        </Route>
+
+        <Route exact path="/user/asdasd">
+          <UserInfo />
+        </Route>
+
+        <Route>
+          <Page404 />
+        </Route>
+      </Router>
     </div>
   );
 }
