df04a3651491c7bf4557700ee10a8273fd0cbe35
Arpit Bajpai <arpitbajpai4321@gmail.com>
2022-11-30 15:40:43 +0530

    Forms Hooks

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
+import { ToastProvider } from 'react-toast-notifications';
+
 import './styles/index.css';
 import { App } from './components';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+ReactDOM.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <ToastProvider autoDismiss autoDismissTimeout={5000} placement="top-left">
+      <App />
+    </ToastProvider>
+  </React.StrictMode>,
+  document.getElementById('root')
 );
-
